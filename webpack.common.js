b5ab5084da024e27a31748f77ce299e1210ba490
Vitalik Moroz <vitalik.moroz2@gmail.com>
2020-09-30 12:31:08 +0300

    update config to propper load all files

--- webpack.common.js
+++ webpack.common.js
@@ -3,23 +3,23 @@ const { resolve } = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-	entry: './src/index.ts',
-	target: 'node',
-	externals: [nodeExternals()],
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'index.js',
-	},
-	resolve: {
-		extensions: ['ts', 'js'],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.ts?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-		],
-	},
+    entry: './src/index.ts',
+    target: 'node',
+    externals: [nodeExternals()],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
 };
